6128d083a7d6dbfc6e6a66e121025c64ecbf7ba0
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-01 16:53:00 +0200

    AGGIUNTO UL LI header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,6 +1,22 @@
 <template>
-    <header>
-
+    <header class="header">
+        <div class="header__logo">
+            <img src="../assets/img/dc-logo.png" alt=""/>
+        </div>
+        <nav class="header__nav">
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -11,5 +27,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.header{
 
+};
 </style>
