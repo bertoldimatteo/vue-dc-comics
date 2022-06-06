bb62de31442152aff3ca1088f63b98c9319ea828
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-06 14:57:41 +0200

    spostato sezione da main a footer

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,6 +1,22 @@
 <template>
     <footer>
         <div class="footer-top">
+            <div class="title">
+                <div class="container">
+                    <h1>Content goes here</h1>
+                </div>
+                </div>
+                <div class="main-footer">
+                <div class="container-secondary shop">
+                    <a href="#"><img src="../assets/img/buy-comics-digital-comics.png" alt=""><span>DIGITAL COMICS</span></a>
+                    <a href="#"><img src="../assets/img/buy-comics-merchandise.png" alt=""><span>DC MERCHANDISE</span></a>
+                    <a href="#"><img src="../assets/img/buy-comics-subscriptions.png" alt=""><span>SUBSCRIPTION</span></a>
+                    <a href="#"><img src="../assets/img/buy-comics-shop-locator.png" alt=""><span>COMIC SHOP LOCATOR</span></a>
+                    <a href="#"><img class="svg" src="../assets/img/buy-dc-power-visa.svg" alt=""><span>DC POWER VISA</span></a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-middle">
             <div class="container">
                 <div class="footer__list">
                     <div class="first__ul">
@@ -77,7 +93,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.footer-top {
+.footer-middle {
     background-image: url('../assets/img/footer-bg.jpg');
     background-repeat: no-repeat;
     background-size: cover;
@@ -102,7 +118,7 @@ export default {
     max-width: 600px;
     max-height: 23.125rem;
 }
-.footer-top a {
+.footer-middle a {
     margin-bottom: .9375rem;
     color: var(--fifth-color);
     text-decoration: none;
@@ -164,5 +180,43 @@ export default {
     flex-direction: column;
 }
 }
+.footer-top {
+    background-color: var(--second-color);
+}
+.title {
+    color: var(--first-color);
+    padding: 50px 0;
+}
+.main-footer {
+    background-color: var(--third-color);
+}
+.footer-top-footer {
+    background-color: var(--third-color);
+}
+.shop { 
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 50px 0;
 
+    & img {
+        height: 3.75rem;
+        max-height: 3.75rem;
+    };
+
+    & .svg {
+        height: 2.5rem;
+    };
+
+    & a {
+        text-decoration: none;
+        color: var(--first-color);
+        display: flex;
+        align-items: center;
+    }
+
+    & span {
+        margin: 1.25rem;
+    }
+}
 </style>
